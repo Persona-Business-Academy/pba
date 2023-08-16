cfec8a28cdfc1c382c769eccd328a40d6f187186
Narek_Boshyan <narek.boshyan@gmail.com>
2023-08-16 09:45:52 +0400

    adding website colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,12 +1,38 @@
 export const blueColors = {
-  'blue.100': '#F6FCFF',
-  'blue.200': '#E2F4FB',
-  'blue.300': '#C3E7F7',
-  'blue.400': '#3CB3E5',
-  'blue.500': '#36A1CE',
-  'blue.600': '#308FB7',
-  'blue.200': '#F6FCFF',
-  'blue.200': '#F6FCFF',
-  'blue.200': '#F6FCFF',
-  'blue.200': '#F6FCFF',
+  'blue.50': '#F6FCFF',
+  'blue.100': '#E2F4FB',
+  'blue.200': '#C3E7F7',
+  'blue.300': '#3CB3E5',
+  'blue.400': '#36A1CE',
+  'blue.500': '#308FB7',
+  'blue.600': '#2D86AC',
+  'blue.700': '#246B89',
+  'blue.800': '#1B5167',
+  'blue.900': '#153F50',
+};
+
+export const violetColors = {
+  'violet.50': '#E9E8ED',
+  'violet.100': '#DDDCE3',
+  'violet.200': '#BAB7C6',
+  'violet.300': '#1F1646',
+  'violet.400': '#1C143F',
+  'violet.500': '#191238',
+  'violet.600': '#171135',
+  'violet.700': '#130D2A',
+  'violet.800': '#0E0A1F',
+  'violet.900': '#0B0819',
+};
+
+export const orangeColors = {
+  'violet.50': '#E9E8ED',
+  'violet.100': '#DDDCE3',
+  'violet.200': '#BAB7C6',
+  'violet.300': '#1F1646',
+  'violet.400': '#1C143F',
+  'violet.500': '#191238',
+  'violet.600': '#171135',
+  'violet.700': '#130D2A',
+  'violet.800': '#0E0A1F',
+  'violet.900': '#0B0819',
 };
