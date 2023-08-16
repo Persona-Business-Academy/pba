513908bcb9fae0364005801c41f71f033aecde9b
Narek_Boshyan <narek.boshyan@gmail.com>
2023-08-16 23:20:09 +0400

    working on customizing chakra ui

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,16 @@ import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider, extendTheme, LightMode } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import { breakpoints, colors, space } from '@/constants/chakra';
 
 const theme = extendTheme({
   config: {
     initialColorMode: 'light',
     useSystemColorMode: false,
   },
+  colors,
+  breakpoints,
+  space,
 });
 
 const queryClient = new QueryClient();
