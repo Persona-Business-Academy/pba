788ec740675da62df92e7891f8300c2b4b711bac
Narek_Boshyan <narek.boshyan@gmail.com>
2023-08-21 10:13:12 +0400

    adding font-family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
-import { Inter } from 'next/font/google';
 import Header from '@/components/organism/Header';
 import { Providers } from './providers';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'PBA Website',
@@ -12,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>
           <Header />
           {children}
