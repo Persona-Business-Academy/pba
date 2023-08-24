7d873f4db8dfc960047e5619831a47a61e985899
Levon Aslanyan <aslanyan2012@mail.ru>
2023-08-24 20:17:49 +0400

    new

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const TermsAndConditions = (props: Props) => {
+  return <div>TermsAndConditions
+    odfd
+  </div>;
+};
+
+export default TermsAndConditions;
