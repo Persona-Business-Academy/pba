26cfb7f4763f6ff577f561d8e4eae521171c25cb
Hayk Kyutneryan <77750836+kyutneryan@users.noreply.github.com>
2023-08-25 20:18:52 +0400

    small fix

--- src/lib/prisma/utils/auth.ts
+++ src/lib/prisma/utils/auth.ts
@@ -1,7 +1,5 @@
-import { NextApiRequest } from 'next';
 import bcrypt from 'bcrypt';
 import { BadRequestException } from 'next-api-decorators';
-import { getToken } from 'next-auth/jwt';
 import { ERROR_MESSAGES } from '@/utils/constants';
 import { findUser } from '../resolvers';
 
@@ -21,14 +19,5 @@ export const validateUserPassword = async (email: string, password: string) => {
     return { id: user.id, email: user.email };
   } catch (e) {
     throw new Error(e as string);
-  }
-};
-
-export const validateJwt = async (req: NextApiRequest) => {
-  try {
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
-    return !!token;
-  } catch {
-    return false;
   }
 };
