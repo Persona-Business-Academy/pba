b57a7c97b057d17992bd31748923d0801396eac5
Narek_Boshyan <narek.boshyan@gmail.com>
2023-08-29 21:27:46 +0400

    working on removing border

--- src/components/organism/index.ts
+++ src/components/organism/index.ts
@@ -2,6 +2,6 @@
 import AuthWrapper from './AuthWrapper';
 import Footer from './Footer';
 import Header from './Header';
-import SimpleSidebar from './ProfileSidebar/index';
+import SimpleSidebar from './ProfileSidebar';
 
 export { AuthWrapper, Header, Footer, SimpleSidebar };
