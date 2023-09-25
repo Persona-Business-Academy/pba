ba59023d2a44be5d3411d2efbc73a38e4f4ccbeb
Hayk Kyutneryan <77750836+kyutneryan@users.noreply.github.com>
2023-09-25 21:43:40 +0400

    fix

--- @types/next-auth.d.ts
+++ @types/next-auth.d.ts
@@ -2,14 +2,14 @@
 import NextAuth, { ISODateString } from 'next-auth';
 
 declare module 'next-auth' {
-  interface Session {
-    expires: ISODateString;
-    token: { email: string; iat: number; jti: string; exp: number };
-    user: any;
-  }
   export interface User {
     id: number;
     email: string;
+  }
+  interface Session {
+    expires: ISODateString;
+    token: { email: string; iat: number; jti: string; exp: number };
+    user: User;
   }
 }
 // todo
