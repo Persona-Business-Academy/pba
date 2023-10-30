639d5ffcd12224c60d4f774e017b05e73fe2b567
Levon Aslanyan <aslanyan2012@mail.ru>
2023-10-30 14:23:10 +0400

    Not Found Mobile

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,11 @@
 'use client';
 import React, { FC } from 'react';
-import { Link } from '@chakra-ui/next-js';
 import {
   Box,
   Container,
-  Grid,
-  GridItem,
+  Flex,
   Heading,
+  Link,
   ListItem,
   Text,
   UnorderedList,
@@ -17,31 +16,41 @@ type Props = {};
 
 const NotFound: FC<Props> = () => {
   return (
-    <Container height="100vh" maxWidth="100%">
-      <Grid templateColumns="1fr 1fr" height="100%" width="100%">
-        <GridItem
-          display="flex"
+    <Container padding="0" maxWidth="1920px">
+      <Flex
+        width="100%"
+        height="100vh"
+        justifyContent="center"
+        flexWrap={{ base: 'wrap', lg: 'nowrap', xl: 'nowrap' }}>
+        <Flex
           flexDirection="column"
-          position="relative"
-          justifyContent="center"
-          height="100vh">
-          <Box paddingLeft="103px" margin="0">
+          justifyContent={{ base: 'center', lg: 'space-between', xl: 'space-between' }}
+          w="960px">
+          <Box
+            // bg="pink"
+            padding={{ base: '0 16px', lg: '0', xl: '0' }}
+            margin={{ base: '36px auto', lg: '0 auto', xl: '0 auto' }}
+            width={{ base: '343px', lg: '359px', xl: '359px' }}
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            flexDirection="column"
+            alignContent="center">
             <Text fontSize="14px" margin="0" paddingBottom="8px" color="#FF6131">
               404 error
             </Text>
             <Heading
               as="h2"
               margin="0"
-              size="44px"
+              size={{ base: '28px', lg: '44px', xl: '44px' }}
               color="#22222"
               fontWeight="700"
-              lineHeight="53.64px">
+              lineHeight={{ base: '34.13px', lg: '53.64px', xl: '53.64px' }}>
               Page not found
             </Heading>
             <Text fontSize="16px" margin="0" paddingBottom="8px" color="#C0C0C0">
-              Sorry, we couldn’t find the page you’re looking for.
+              Sorry, we couldnt find the page youre looking for.
             </Text>
-
             <Link
               href="!#"
               display="flex"
@@ -50,50 +59,47 @@ const NotFound: FC<Props> = () => {
               alignItems="center"
               textDecorationLine="none">
               Go back home
-              <Image src="/icons/not_found_icon.svg" alt="Icon" width="24" height="24"></Image>
+              <Image src="/icons/not_found_icon.svg" alt="Icon" width={24} height={24} />
             </Link>
           </Box>
 
-          <UnorderedList
-            padding="64px 0 64px 103px"
-            margin="0"
+          <Box
             width="100%"
-            listStyleType="none"
-            bg="#D1D5DB"
-            display="flex"
-            fontSize="14px"
-            position="absolute"
-            bottom="0">
-            <ListItem lineHeight="20px" marginRight="16px" display="flex" alignItems="center">
-              <Link href="!#" color="#6B7280">
-                Contact support
-              </Link>
-              <Box width="1px" height="20px" marginLeft="16px" bg="black"></Box>
-            </ListItem>
-
-            <ListItem
-              lineHeight="20px"
-              marginRight="16px"
+            bg="#E9E8ED"
+            padding={{ base: '8px 16px', lg: '64px 32px', xl: '64px 32px' }}
+            margin={{ base: '0 0 36px 0', lg: '0', xl: '0' }}>
+            <UnorderedList
+              margin="0 auto"
+              listStyleType="none"
               color="#6B7280"
               display="flex"
-              alignItems="center">
-              <Link href="!#">Contact support</Link>
-              <Box width="1px" height="20px" marginLeft="16px" bg="black" />
-            </ListItem>
-            <ListItem lineHeight="20px" color="#6B7280">
-              <Link href="!#">Contact support</Link>
-            </ListItem>
-          </UnorderedList>
-        </GridItem>
+              fontSize="14px"
+              justifyContent="center">
+              <ListItem lineHeight="20px" marginRight="16px" display="flex" alignItems="center">
+                <Link href="!#">Contact support</Link>
+                <Box width="1px" height="20px" marginLeft="16px" bg="#D1D5DB"></Box>
+              </ListItem>
 
-        <GridItem
-          height="100%"
-          width="100%"
-          backgroundImage="/images/public_available/not_found_img.jpg"
-          backgroundRepeat="no-repeat"
-          backgroundPosition="center"
-          backgroundSize="cover"></GridItem>
-      </Grid>
+              <ListItem lineHeight="20px" marginRight="16px" display="flex" alignItems="center">
+                <Link href="!#">Help center</Link>
+                <Box width="1px" height="20px" marginLeft="16px" bg="#D1D5DB" />
+              </ListItem>
+
+              <ListItem lineHeight="20px">
+                <Link href="!#">Privacy policy</Link>
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Flex>
+        <Box width="960px" height={{ base: '419px', lg: '100vh', xl: '100vh' }}>
+          <Box
+            minHeight="100%"
+            backgroundImage="/images/public_available/not_found_img.jpg"
+            backgroundSize="cover"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"></Box>
+        </Box>
+      </Flex>
     </Container>
   );
 };
