2506c9095b246f6de28885fbcefb075718817bfa
Asya Harutyunyan <asya.harutyunyan010@gmail.com>
2023-11-24 20:32:16 +0400

    achievements not ready yet

--- src/app/(with_Header)/profile/achievements/page.tsx
+++ src/app/(with_Header)/profile/achievements/page.tsx
@@ -1,9 +1,183 @@
 import React, { FC } from 'react';
+import { Box, Container, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { montserrat } from '@/utils/constants/fonts';
 
 type Props = {};
 
 const Achievements: FC<Props> = () => {
-  return <div>Achievements</div>;
+  return (
+    <Container maxW="1138px" margin="0 auto" p={{ base: '16px', md: '0' }}>
+      <Heading
+        m="64px 0"
+        textAlign="center"
+        fontSize={{ base: '28px', md: '44px' }}
+        color="#000000"
+        fontStyle="normal"
+        lineHeight="normal"
+        fontWeight={{ base: 600, md: 700 }}>
+        Your achievements
+      </Heading>
+      <Flex flexDirection="column" gap={{ base: '16px', md: '28px' }}>
+        <Flex
+          width="100%"
+          flexDirection={{ base: 'column', xl: 'row' }}
+          borderRadius="12px"
+          boxShadow="0px 4px 6px 0px rgba(0, 0, 0, 0.06)"
+          gap="29px"
+          p="16px">
+          <Box
+            width="296px"
+            height="244px"
+            borderRadius="6px"
+            overflow="hidden"
+            display={{ base: 'none', sm: 'block' }}>
+            <Image
+              src="/images/public_available/achievements_img.jpg"
+              alt="Achievements image"
+              width={296}
+              height={244}
+            />
+          </Box>
+          <Box
+            width="781px"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center">
+            <Flex gap={{ base: '28px', lg: '136px' }}>
+              <Box height="105px">
+                <Text
+                  textAlign="center"
+                  fontSize={{ base: '24px', md: '32px' }}
+                  fontStyle="normal"
+                  fontWeight={700}
+                  lineHeight="normal"
+                  color="#222">
+                  Interior design
+                </Text>
+                <Box m="24px 0" borderRadius="23px">
+                  <div
+                    style={{
+                      width: '427px',
+                      height: '14px',
+                      backgroundColor: '#F6FCFF',
+                      borderRadius: '23px',
+                    }}>
+                    <div
+                      style={{
+                        width: '135px',
+                        height: '14px',
+                        backgroundColor: '#3CB4E7',
+                        borderRadius: '23px',
+                      }}></div>
+                  </div>
+                </Box>
+              </Box>
+
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                flexDirection="column">
+                <div
+                  style={{
+                    borderRadius: '50%',
+                    border: '4px solid #30A883',
+                    width: '110px',
+                    height: '110px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}>
+                  <Text
+                    className={montserrat.className}
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    fontSize="44px"
+                    fontStyle="normal"
+                    fontWeight={700}
+                    lineHeight="normal"
+                    color="#30A883">
+                    75
+                  </Text>
+                </div>
+                <Text
+                  fontSize="24px"
+                  fontStyle="normal"
+                  fontWeight={700}
+                  lineHeight="normal"
+                  color="#222"
+                  m="16px 0 0 0">
+                  Very good
+                </Text>
+              </Box>
+            </Flex>
+            <Flex gap="54px" mb="24px">
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center">
+                <Image src="/icons/time_icon.svg" width={46} height={46} alt="time" />
+                <Text
+                  m="8px 0 0 0"
+                  fontSize="16px"
+                  fontStyle="normal"
+                  fontWeight={400}
+                  lineHeight="22px"
+                  color="#222">
+                  1 hour 20 minute
+                </Text>
+              </Box>
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center">
+                <Image src="/icons/level_icon.svg" width={46} height={46} alt="level" />
+                <Text
+                  m="8px 0 0 0"
+                  fontSize="16px"
+                  fontStyle="normal"
+                  fontWeight={400}
+                  lineHeight="22px"
+                  color="#222">
+                  1 hour 20 minute
+                </Text>
+              </Box>
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center">
+                <Image src="/icons/book_icon.svg" width={46} height={46} alt="lessons" />
+                <Text
+                  m="8px 0 0 0"
+                  fontSize="16px"
+                  fontStyle="normal"
+                  fontWeight={400}
+                  lineHeight="22px"
+                  color="#222">
+                  1 hour 20 minute
+                </Text>
+              </Box>
+            </Flex>
+            <Link
+              href="#"
+              target="blank"
+              fontSize="16px"
+              fontStyle="normal"
+              fontWeight={400}
+              lineHeight="normal"
+              textDecoration="underline">
+              Download certificate PDF
+            </Link>
+          </Box>
+        </Flex>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Achievements;
