28e7f7ac79da5e03eef29f2209980289d5065c74
Hayk Kyutneryan <77750836+kyutneryan@users.noreply.github.com>
2023-12-16 19:44:54 +0400

    added change password

--- src/pages/api/user/[[...params]].ts
+++ src/pages/api/user/[[...params]].ts
@@ -1,16 +1,28 @@
-import * as client from '@prisma/client';
-import { Body, Catch, createHandler, Post } from 'next-api-decorators';
+import { Body, Catch, createHandler, Post, ValidationPipe } from 'next-api-decorators';
+import { User } from 'next-auth';
 import { CurrentUser } from '@/lib/prisma/decorators/current-user.decorator';
 import { exceptionHandler } from '@/lib/prisma/error';
 import { AuthMiddleware } from '@/lib/prisma/middlewares/auth-middleware';
 import { UserResolver } from '@/lib/prisma/resolvers';
+import { ChangePasswordValidation, UserProfileFormValidation } from '@/utils/validation';
 
 @Catch(exceptionHandler)
 @AuthMiddleware()
 class UserHandler {
   @Post('/update-profile')
-  async _updateUserProfile(@Body() body: any, @CurrentUser() user: client.User) {
+  updateUserProfile(
+    @Body(ValidationPipe) body: UserProfileFormValidation,
+    @CurrentUser() user: NonNullable<User>,
+  ) {
     return UserResolver.updateUserProfile(body, user);
+  }
+
+  @Post('/update-password')
+  changeUserPassword(
+    @Body(ValidationPipe) body: ChangePasswordValidation,
+    @CurrentUser() user: NonNullable<User>,
+  ) {
+    return UserResolver.changeUserPassword(body, user);
   }
 }
 
